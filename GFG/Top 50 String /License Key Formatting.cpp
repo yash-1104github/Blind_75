c083499c823b88e9d9b6e5c10d50bbaca1a0d674
Yash Gupta <gyash1104@gmail.com>
2024-10-14 23:15:14 +0530

    Created License Key Formatting.cpp

--- GFG/Top 50 String /License Key Formatting.cpp
+++ GFG/Top 50 String /License Key Formatting.cpp
@@ -1,32 +1,25 @@
 class Solution
 {
    public:
-    string ReFormatString(string S, int K){
-    	
+    string ReFormatString(string S, int K){    	
      	int n = S.size();
-        string temp = "";
-        
-        for(int i = 0 ; i< n ;i++){
-            
+        string temp = "";        
+        for(int i = 0 ; i< n ;i++){            
             if(S[i] != '-'){
              temp += toupper(S[i]);
             }
-        }
-     
+        }     
         string ans = "";
-        int cnt = 1;
-        
+        int cnt = 0;     
         for(int i = temp.size()-1; i >= 0; i--){
-            
-            if(cnt == K+1 ){
+            if(cnt == K){
                 ans += '-';
-                cnt = 1;
+                cnt = 0;
             }
               ans += temp[i];
               cnt++;
         }
-      
-        reverse(ans.begin(),ans.end());
-    	return ans;
+         reverse(ans.begin(),ans.end());
+      	return ans;
     }
 };
