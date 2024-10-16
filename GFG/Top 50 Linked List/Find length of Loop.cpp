ab8efedd091f3647e011a53322ef34381ec6bc4c
Yash Gupta <gyash1104@gmail.com>
2024-10-16 23:42:46 +0530

    Create Find length of Loop.cpp

---
+++ GFG/Top 50 Linked List/Find length of Loop.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        if(head == NULL){
+            return 0;
+        }
+        
+    //  Node* slow = head;
+    //  Node* fast = head->next;
+      
+    //   int cnt = 1;
+      
+    //  while(fast != nullptr && fast->next != NULL){
+          
+    //      if(slow->data == fast->data && ){
+    //          Node* curr = slow->next;
+            
+    //     while(curr -> data != slow->data){
+    //           cnt++;
+    //           curr = curr->next;
+    //       }            
+    //         return cnt;
+    //     }
+        
+    //      slow = slow->next;
+    //      fast = fast->next->next;
+    //  }
+    
+    
+    unordered_map<Node*, int>mp;
+    
+      int cnt = 1;
+      while(head){
+          
+        if(mp.find(head) != mp.end()){
+            return cnt - mp[head];
+        }
+        
+        mp[head] =  cnt;
+        cnt++;
+        head = head-> next;
+        
+     }
+    
+    
+      return 0;  
+    }
+};
