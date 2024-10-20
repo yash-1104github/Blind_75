7a5832355fa4fdef55f31816be07dade3f289e04
Yash Gupta <gyash1104@gmail.com>
2024-10-20 22:17:49 +0530

    Create Find the first node of loop in linked list.cpp

---
+++ GFG/Top 50 Linked List/Find the first node of loop in linked list.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    // Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head) {
+        // your code here
+        Node* slow = head;
+        Node* fast = head;
+      
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+              if(slow == fast){
+                break;
+            }
+        }
+        
+        if(slow != fast) return -1;
+        slow = head;
+        
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next; 
+        }
+        return slow->data;
+    }
+};
