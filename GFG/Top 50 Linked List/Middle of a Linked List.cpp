547d3f7763adda71efddc5aca732792e1626717c
Yash Gupta <gyash1104@gmail.com>
2024-10-25 23:05:23 +0530

    Create Middle of a Linked List.cpp

---
+++ GFG/Top 50 Linked List/Middle of a Linked List.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    /* Should return data of middle node. If linked list is empty, then -1 */
+    int getMiddle(Node* head) {
+        // code here
+        
+        if(head->next == NULL) return head->data;
+        
+        Node* slow = head;
+        Node* fast = head;
+        
+        while(fast != NULL && fast->next != NULL){
+            
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            
+        }
+        
+        return slow->data;
+    }
+};
