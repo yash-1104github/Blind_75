8cb27c26df54171e8b612e914d556e25a2b55868
Yash Gupta <gyash1104@gmail.com>
2024-11-15 15:16:51 +0530

    Create Implement stack using array.cpp

---
+++ Love Baber DSA sheet/Implement stack using array.cpp
@@ -0,0 +1,21 @@
+void MyStack ::push(int x) {
+    // Your Code
+    top = top+1;
+    arr[top] = x;
+    
+    
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    // Your Code
+    if(top == -1){
+        return -1;
+    }
+    
+    int x = arr[top];
+    top--;
+    return x;
+    
+    
+}
